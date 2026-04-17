0d809924d9f5ef888e1b2cea739ec6a5ec85fee4
Eman ._. <valenciaemanuel472@gmail.com>
2026-04-17 15:39:23 -0600

    Delete frontend directory

--- frontend/src/api/asistente.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-import { getAuthHeaders } from './auth';
-import { API_BASE_URL } from './baseUrl';
-
-const API_URL = `${API_BASE_URL}/api`;
-
-export const sendAssistantMessage = (payload) => {
-  return axios.post(`${API_URL}/asistente/chat`, payload, getAuthHeaders());
-};
