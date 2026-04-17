18f9f3b8f8e5a9fd143d232d793bc76455fba8c3
Eman ._. <valenciaemanuel472@gmail.com>
2026-04-17 15:36:55 -0600

    Delete backend directory

--- backend/routes/asistente.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { chatAsistente } from '../controllers/asistenteController.js';
-import { protect } from '../middleware/authMiddleware.js';
-
-const router = Router();
-
-router.post('/chat', protect, chatAsistente);
-
-export default router;
